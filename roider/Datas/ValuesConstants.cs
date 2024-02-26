3ce99391d1f4fdefdf7c0dda70f7d4486da55f80
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-02-26 14:07:44 +0545

    started the project and added models

---
+++ roider/Datas/ValuesConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace roider.Datas;
+
+public class ValuesConstants
+{
+    public const string DbString = $"User Id=orc;Password=password;Data Source=localhost:1521/orcl;";
+}
---
+++ roider/Models/Students.cs
@@ -0,0 +1,13 @@
+﻿namespace roider.Models;
+
+public class Students
+{
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+    public string Contact { get; set; }
+    public DateTime DOB { get; set; }
+    public string EmailAddress { get; set; }
+    public string Country { get; set; }
+
+    public List<Students> StudentsList = [];
+}
