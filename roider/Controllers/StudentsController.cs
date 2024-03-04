2eee9d22dc7b9b2effd53553d48dfd213c3f85b9
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-04 08:53:43 +0545

    Add StudentsController to manage student data

---
+++ roider/Controllers/StudentsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Oracle.ManagedDataAccess.Client;
+using roider.Models;
+
+namespace roider.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly Students _studentsModel;
+
+        public StudentsController()
+        {
+            _studentsModel = new Students();
+        }
+
+        // GET: Students
+        public IActionResult Index()
+        {
+            List<Students> studentsList = _studentsModel.FetchStudents();
+            return View(studentsList);
+        }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Students student)
+        {
+            if (ModelState.IsValid)
+            {
+                _studentsModel.AddStudent(student);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            var student = _studentsModel.FetchStudentById(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Students student)
+        {
+            if (id != student.StudentID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _studentsModel.EditStudentDetails(student, id);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            var student = _studentsModel.FetchStudentById(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _studentsModel.DeleteStudent(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Students/Details/5
+        public IActionResult Details(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            var student = _studentsModel.FetchStudentById(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+    }
+}
