ee14e2ebcdd5c0803a7c6be1830a377512c25f40
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-04 08:54:30 +0545

    Add Progresses class methods for CRUD operations

--- roider/Models/Progresses.cs
+++ roider/Models/Progresses.cs
@@ -1,7 +1,12 @@
-﻿namespace roider.Models;
+﻿using System.Data;
+using Oracle.ManagedDataAccess.Client;
+using roider.Datas;
+
+namespace roider.Models;
 
 public class Progresses
 {
+    public List<Progresses> ProgressesList = [];
     public int ProgressID { get; set; }
     public int StudentID { get; set; }
     public int LessonID { get; set; }
@@ -10,6 +15,147 @@ public class Progresses
 
     public Students Student { get; set; }
     public Lessons Lesson { get; set; }
+    
+    public void AddProgress(Progresses progress)
+    {
+        try
+        {
+            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            {
+                string queryString = "INSERT INTO Progresses (ProgressID, StudentID, LessonID, LessonStatus, LastAccessedDate) VALUES (:ProgressID, :StudentID, :LessonID, :LessonStatus, :LastAccessedDate)";
+                OracleCommand cmd = new OracleCommand(queryString, con);
+                cmd.Parameters.Add("ProgressID", OracleDbType.Int32).Value = progress.ProgressID;
+                cmd.Parameters.Add("StudentID", OracleDbType.Int32).Value = progress.StudentID;
+                cmd.Parameters.Add("LessonID", OracleDbType.Int32).Value = progress.LessonID;
+                cmd.Parameters.Add("LessonStatus", OracleDbType.Varchar2).Value = progress.LessonStatus;
+                cmd.Parameters.Add("LastAccessedDate", OracleDbType.Date).Value = progress.LastAccessedDate;
 
-    public List<Progresses> ProgressesList = [];
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    public void EditProgress(Progresses progress, int oldProgressId)
+    {
+        try
+        {
+            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            {
+                string queryString = "UPDATE Progresses SET StudentID = :StudentID, LessonID = :LessonID, LessonStatus = :LessonStatus, LastAccessedDate = :LastAccessedDate WHERE ProgressID = :OldProgressID";
+                OracleCommand cmd = new OracleCommand(queryString, con);
+                cmd.Parameters.Add("StudentID", OracleDbType.Int32).Value = progress.StudentID;
+                cmd.Parameters.Add("LessonID", OracleDbType.Int32).Value = progress.LessonID;
+                cmd.Parameters.Add("LessonStatus", OracleDbType.Varchar2).Value = progress.LessonStatus;
+                cmd.Parameters.Add("LastAccessedDate", OracleDbType.Date).Value = progress.LastAccessedDate;
+                cmd.Parameters.Add("OldProgressID", OracleDbType.Int32).Value = oldProgressId;
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    public void DeleteProgress(int progressId)
+    {
+        try
+        {
+            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            {
+                string queryString = "DELETE FROM Progresses WHERE ProgressID = :ProgressID";
+                OracleCommand cmd = new OracleCommand(queryString, con);
+                cmd.Parameters.Add("ProgressID", OracleDbType.Int32).Value = progressId;
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public List<Progresses> FetchProgresses()
+    {
+        List<Progresses> progressesList = new List<Progresses>();
+        try
+        {
+            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            {
+                string queryString = "SELECT ProgressID, StudentID, LessonID, LessonStatus, LastAccessedDate FROM Progresses";
+                OracleCommand cmd = new OracleCommand(queryString, con);
+                cmd.BindByName = true;
+                cmd.CommandType = CommandType.Text;
+
+                con.Open();
+                OracleDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Progresses progress = new Progresses
+                    {
+                        ProgressID = reader.GetInt32(0),
+                        StudentID = reader.GetInt32(1),
+                        LessonID = reader.GetInt32(2),
+                        LessonStatus = reader.GetString(3),
+                        LastAccessedDate = reader.GetDateTime(4)
+                    };
+                    progressesList.Add(progress);
+                }
+                reader.Dispose();
+                con.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return progressesList;
+    }
+    public Progresses FetchProgressById(int progressId)
+    {
+        Progresses progress = null;
+        try
+        {
+            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            {
+                string queryString = "SELECT ProgressID, StudentID, LessonID, LessonStatus, LastAccessedDate FROM Progresses WHERE ProgressID = :ProgressID";
+                OracleCommand cmd = new OracleCommand(queryString, con);
+                cmd.Parameters.Add("ProgressID", OracleDbType.Int32).Value = progressId;
+                cmd.BindByName = true;
+                cmd.CommandType = CommandType.Text;
+
+                con.Open();
+                OracleDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    progress = new Progresses
+                    {
+                        ProgressID = reader.GetInt32(0),
+                        StudentID = reader.GetInt32(1),
+                        LessonID = reader.GetInt32(2),
+                        LessonStatus = reader.GetString(3),
+                        LastAccessedDate = reader.GetDateTime(4)
+                    };
+                }
+                reader.Dispose();
+                con.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return progress;
+    }
+
 }
