3219773f95282178267dd3edb444437cb3f5dd2a
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-04 08:53:11 +0545

    Add CoursesController with CRUD operations

---
+++ roider/Controllers/CoursesController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Oracle.ManagedDataAccess.Client;
+using roider.Datas;
+using roider.Models;
+using System.Collections.Generic;
+
+namespace roider.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly Courses _coursesModel;
+
+        public CoursesController()
+        {
+            _coursesModel = new Courses();
+        }
+
+        // GET: Courses
+        public IActionResult Index()
+        {
+            List<Courses> coursesList = _coursesModel.FetchCourses();
+            return View(coursesList);
+        }
+
+        // GET: Courses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Courses/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Courses course)
+        {
+            if (ModelState.IsValid)
+            {
+                _coursesModel.AddCourse(course);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(course);
+        }
+
+        // GET: Courses/Edit/5
+        public IActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+        
+            var course = _coursesModel.FetchCourseById(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            return View(course);
+        }
+        //
+        // // POST: Courses/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(string id, Courses course)
+        {
+            if (id != course.CourseID)
+            {
+                return NotFound();
+            }
+        
+            if (ModelState.IsValid)
+            {
+                _coursesModel.EditCourse(course, id);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(course);
+        }
+
+        // GET: Courses/Delete/5
+        public IActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+        
+            var course = _coursesModel.FetchCourseById(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+        
+            return View(course);
+        }
+
+        // POST: Courses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(string id)
+        {
+            _coursesModel.DeleteCourse(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Courses/Details/5
+        public IActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+        
+            var course = _coursesModel.FetchCourseById(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+        
+            return View(course);
+        }
+    }
+}
