3a05aa9b35035e41a0b65352bae4d47ab42e3826
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-16 19:30:58 +0545

    Update project files and dependencies

--- roider/obj/Debug/net8.0/roider.AssemblyInfo.cs
+++ roider/obj/Debug/net8.0/roider.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("roider")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03263d374a79a2a3ee40bca64fa3268ba8a3b807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b18fffdf30e5a38808a355daefa318baae4639e")]
 [assembly: System.Reflection.AssemblyProductAttribute("roider")]
 [assembly: System.Reflection.AssemblyTitleAttribute("roider")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
