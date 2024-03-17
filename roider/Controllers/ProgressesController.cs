46bd0aac19820ea608cea571efa9efcad777862a
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-17 13:08:45 +0545

    Add async/await to ProgressesController methods

--- roider/Controllers/ProgressesController.cs
+++ roider/Controllers/ProgressesController.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Oracle.ManagedDataAccess.Client;
 using roider.Models;
 
 namespace roider.Controllers
@@ -13,7 +13,7 @@ namespace roider.Controllers
         // GET: Progresses
         public IActionResult Index()
         {
-            Task<List<Progresses>> progressesList = _progressesModel.FetchProgressesAsync();
+            List<Progresses> progressesList = _progressesModel.FetchProgressesAsync().Result;
             return View(progressesList);
         }
 
@@ -21,43 +21,50 @@ namespace roider.Controllers
         public IActionResult Create()
         {
             var students = new Students().GetStudents();
+            var lessons = new Lessons().FetchLessons();
             ViewBag.StudentsList = students;
+            ViewBag.LessonsList = lessons;
             return View();
         }
 
         // POST: Progresses/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Progresses progress)
+        public async Task<IActionResult> Create(Progresses progress)
         {
             if (ModelState.IsValid)
             {
-                _progressesModel.AddProgressAsync(progress);
+                await _progressesModel.AddProgressAsync(progress);
                 return RedirectToAction(nameof(Index));
             }
             return View(progress);
         }
 
         // GET: Progresses/Edit/5
-        public IActionResult Edit(int id)
+        public async Task<IActionResult> Edit(int id)
         {
             if (id == 0)
             {
                 return NotFound();
             }
 
-            var progress = _progressesModel.FetchProgressById(id);
+            var progress = await _progressesModel.FetchProgressByIdAsync(id);
             if (progress == null)
             {
                 return NotFound();
             }
+
+            var students = new Students().GetStudents();
+            var lessons = new Lessons().FetchLessons();
+            ViewBag.StudentsList = students;
+            ViewBag.LessonsList = lessons;
             return View(progress);
         }
 
         // POST: Progresses/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, Progresses progress)
+        public async Task<IActionResult> Edit(int id, Progresses progress)
         {
             if (id != progress.ProgressId)
             {
@@ -66,21 +73,21 @@ namespace roider.Controllers
 
             if (ModelState.IsValid)
             {
-                _progressesModel.EditProgressAsync(progress, id);
+                await _progressesModel.EditProgressAsync(progress);
                 return RedirectToAction(nameof(Index));
             }
             return View(progress);
         }
 
         // GET: Progresses/Delete/5
-        public IActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
             if (id == 0)
             {
                 return NotFound();
             }
 
-            var progress = _progressesModel.FetchProgressById(id);
+            var progress = await _progressesModel.FetchProgressByIdAsync(id);
             if (progress == null)
             {
                 return NotFound();
@@ -92,21 +99,21 @@ namespace roider.Controllers
         // POST: Progresses/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            _progressesModel.DeleteProgress(id);
+            await _progressesModel.DeleteProgressAsync(id);
             return RedirectToAction(nameof(Index));
         }
 
         // GET: Progresses/Details/5
-        public IActionResult Details(int id)
+        public async Task<IActionResult> Details(int id)
         {
             if (id == 0)
             {
                 return NotFound();
             }
 
-            var progress = _progressesModel.FetchProgressById(id);
+            var progress = await _progressesModel.FetchProgressByIdAsync(id);
             if (progress == null)
             {
                 return NotFound();
@@ -114,5 +121,22 @@ namespace roider.Controllers
 
             return View(progress);
         }
+        public async Task<IActionResult> SearchLessons(string searchTerm)
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm))
+            {
+                return NotFound();
+            }
+
+            var progresses = await _progressesModel.SearchProgressesAsync(searchTerm);
+
+            if (progresses == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView("_ProgressList", progresses);
+        }
+
     }
 }
