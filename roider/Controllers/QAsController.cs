b82a47053abe2d218266622f43d3b39c69016bfd
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-19 22:35:58 +0545

    Refactor QAsController to improve code structure and maintainability

--- roider/Controllers/QAsController.cs
+++ roider/Controllers/QAsController.cs
@@ -1,141 +1,113 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using Oracle.ManagedDataAccess.Client;
+﻿using Microsoft.AspNetCore.Mvc;
 using roider.Models;
 
-namespace roider.Controllers
+namespace roider.Controllers;
+
+public class QAsController : Controller
 {
-    public class QAsController : Controller
+    private readonly QAs _qasModel = new();
+
+    // GET: QAs
+    public IActionResult Index()
     {
-        private readonly QAs _qasModel = new();
+        var qasList = _qasModel.FetchQAs();
+        return View(qasList);
+    }
 
-        // GET: QAs
-        public IActionResult Index()
+    // GET: QAs/Create
+    public IActionResult Create()
+    {
+        var courses = new Courses().FetchCourses();
+        ViewBag.CoursesList = courses;
+        var students = new Students().GetStudents();
+        ViewBag.StudentsList = students;
+        return View();
+    }
+
+    // POST: QAs/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(QAs qa)
+    {
+        if (ModelState.IsValid)
         {
-            List<QAs> qasList = _qasModel.FetchQAs();
-            return View(qasList);
-        }
-
-        // GET: QAs/Create
-        public IActionResult Create()
-        {
-            var courses = new Courses().FetchCourses();
-            ViewBag.CoursesList = courses;
-            var students = new Students().GetStudents();
-            ViewBag.StudentsList = students;
-            return View();
-        }
-
-        // POST: QAs/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(QAs qa)
-        {
-            if (ModelState.IsValid)
-            {
-                _qasModel.AddQa(qa);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(qa);
-        }
-
-        // GET: QAs/Edit/5
-        public IActionResult Edit(int id)
-        {
-            if (id == 0)
-            {
-                return NotFound();
-            }
-
-            var qa = _qasModel.FetchQaById(id);
-            if (qa == null)
-            {
-                return NotFound();
-            }
-            var courses = new Courses().FetchCourses();
-            ViewBag.CoursesList = courses;
-            var students = new Students().GetStudents();
-            ViewBag.StudentsList = students;
-            return View(qa);
-        }
-
-        // POST: QAs/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, QAs qa)
-        {
-            if (id != qa.Qaid)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                _qasModel.EditQa(qa, id);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(qa);
-        }
-
-        // GET: QAs/Delete/5
-        public IActionResult Delete(int id)
-        {
-            if (id == 0)
-            {
-                return NotFound();
-            }
-
-            var qa = _qasModel.FetchQaById(id);
-            if (qa == null)
-            {
-                return NotFound();
-            }
-
-            return View(qa);
-        }
-
-        // POST: QAs/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            _qasModel.DeleteQa(id);
+            _qasModel.AddQa(qa);
             return RedirectToAction(nameof(Index));
         }
 
-        // GET: QAs/Details/5
-        public IActionResult Details(int id)
+        return View(qa);
+    }
+
+    // GET: QAs/Edit/5
+    public IActionResult Edit(int id)
+    {
+        if (id == 0) return NotFound();
+
+        var qa = _qasModel.FetchQaById(id);
+        if (qa == null) return NotFound();
+        var courses = new Courses().FetchCourses();
+        ViewBag.CoursesList = courses;
+        var students = new Students().GetStudents();
+        ViewBag.StudentsList = students;
+        return View(qa);
+    }
+
+    // POST: QAs/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, QAs qa)
+    {
+        if (id != qa.Qaid) return NotFound();
+
+        if (ModelState.IsValid)
         {
-            if (id == 0)
-            {
-                return NotFound();
-            }
-
-            var qa = _qasModel.FetchQaById(id);
-            if (qa == null)
-            {
-                return NotFound();
-            }
-
-            return View(qa);
-        }
-        public async Task<IActionResult> SearchQAs(string searchTerm)
-        {
-            if (string.IsNullOrWhiteSpace(searchTerm))
-            {
-                return NotFound();
-            }
-
-            var qas = await _qasModel.SearchQAsAsync(searchTerm);
-
-            if (qas == null)
-            {
-                return NotFound();
-            }
-
-            return PartialView("_QAsList", qas);
+            _qasModel.EditQa(qa, id);
+            return RedirectToAction(nameof(Index));
         }
 
+        return View(qa);
+    }
+
+    // GET: QAs/Delete/5
+    public IActionResult Delete(int id)
+    {
+        if (id == 0) return NotFound();
+
+        var qa = _qasModel.FetchQaById(id);
+        if (qa == null) return NotFound();
+
+        return View(qa);
+    }
+
+    // POST: QAs/Delete/5
+    [HttpPost]
+    [ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        _qasModel.DeleteQa(id);
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: QAs/Details/5
+    public IActionResult Details(int id)
+    {
+        if (id == 0) return NotFound();
+
+        var qa = _qasModel.FetchQaById(id);
+        if (qa == null) return NotFound();
+
+        return View(qa);
+    }
+
+    public async Task<IActionResult> SearchQAs(string searchTerm)
+    {
+        if (string.IsNullOrWhiteSpace(searchTerm)) return NotFound();
+
+        var qas = await _qasModel.SearchQAsAsync(searchTerm);
+
+        if (qas == null) return NotFound();
+
+        return PartialView("_QAsList", qas);
     }
 }
--- roider/Models/QA.cs
+++ roider/Models/QA.cs
@@ -1,30 +1,31 @@
 ﻿using System.Data;
 using Oracle.ManagedDataAccess.Client;
 using roider.Datas;
-using roider.Models;
 
 namespace roider.Models;
+
 public class QAs
 {
     public List<QAs> QAsList = [];
-    public int Qaid { get; init; }
-    public int StudentId { get; init; }
-    public string CourseId { get; init; }
-    public string Question { get; init; }
-    public string Answer { get; init; }
-    public DateTime FeedbackDate { get; init; }
-    
+    public int Qaid { get; set; }
+    public int StudentId { get; set; }
+    public string CourseId { get; set; }
+    public string Question { get; set; }
+    public string Answer { get; set; }
+    public DateTime FeedbackDate { get; set; }
+
     public Students? Student { get; set; }
     public Courses? Course { get; set; }
-    
+
     public void AddQa(QAs qa)
     {
         try
         {
-            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            using (var con = new OracleConnection(ValuesConstants.DbString))
             {
-                string queryString = "INSERT INTO QAs (Qaid, StudentId, CourseID, Question, Answer, FeedbackDate) VALUES (:Qaid, :StudentId, :CourseID, :Question, :Answer, :FeedbackDate)";
-                OracleCommand cmd = new OracleCommand(queryString, con);
+                var queryString =
+                    "INSERT INTO QA (Qaid, StudentId, CourseID, Question, Answer, FeedbackDate) VALUES (:Qaid, :StudentId, :CourseID, :Question, :Answer, :FeedbackDate)";
+                var cmd = new OracleCommand(queryString, con);
                 cmd.Parameters.Add("Qaid", OracleDbType.Int32).Value = qa.Qaid;
                 cmd.Parameters.Add("StudentId", OracleDbType.Int32).Value = qa.StudentId;
                 cmd.Parameters.Add("CourseID", OracleDbType.Varchar2).Value = qa.CourseId;
@@ -42,14 +43,16 @@ public class QAs
             Console.WriteLine(ex.Message);
         }
     }
+
     public void EditQa(QAs qa, int oldQaId)
     {
         try
         {
-            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            using (var con = new OracleConnection(ValuesConstants.DbString))
             {
-                string queryString = "UPDATE QAs SET StudentId = :StudentId, CourseID = :CourseID, Question = :Question, Answer = :Answer, FeedbackDate = :FeedbackDate WHERE Qaid = :OldQaid";
-                OracleCommand cmd = new OracleCommand(queryString, con);
+                var queryString =
+                    "UPDATE QA SET StudentId = :StudentId, CourseID = :CourseID, Question = :Question, Answer = :Answer, FeedbackDate = :FeedbackDate WHERE Qaid = :OldQaid";
+                var cmd = new OracleCommand(queryString, con);
                 cmd.Parameters.Add("StudentId", OracleDbType.Int32).Value = qa.StudentId;
                 cmd.Parameters.Add("CourseID", OracleDbType.Varchar2).Value = qa.CourseId;
                 cmd.Parameters.Add("Question", OracleDbType.Varchar2).Value = qa.Question;
@@ -72,10 +75,10 @@ public class QAs
     {
         try
         {
-            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            using (var con = new OracleConnection(ValuesConstants.DbString))
             {
-                string queryString = "DELETE FROM QAs WHERE Qaid = :Qaid";
-                OracleCommand cmd = new OracleCommand(queryString, con);
+                var queryString = "DELETE FROM QA WHERE Qaid = :Qaid";
+                var cmd = new OracleCommand(queryString, con);
                 cmd.Parameters.Add("Qaid", OracleDbType.Int32).Value = qaId;
 
                 con.Open();
@@ -88,33 +91,60 @@ public class QAs
             Console.WriteLine(ex.Message);
         }
     }
+
     public List<QAs> FetchQAs()
     {
-        List<QAs> qasList = new List<QAs>();
+        var qasList = new List<QAs>();
         try
         {
-            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            using (var con = new OracleConnection(ValuesConstants.DbString))
             {
-                string queryString = "SELECT Qaid, StudentId, CourseID, Question, Answer, FeedbackDate FROM QAs";
-                OracleCommand cmd = new OracleCommand(queryString, con);
+                var queryString = @"
+                SELECT 
+                    q.Qaid, 
+                    q.StudentId, 
+                    s.StudentName, 
+                    q.CourseID, 
+                    c.CourseTitle, 
+                    q.Question, 
+                    q.Answer, 
+                    q.FeedbackDate
+                FROM 
+                    QA q
+                JOIN 
+                    Students s ON q.StudentId = s.StudentId
+                JOIN 
+                    Courses c ON q.CourseID = c.CourseID";
+                var cmd = new OracleCommand(queryString, con);
                 cmd.BindByName = true;
                 cmd.CommandType = CommandType.Text;
 
                 con.Open();
-                OracleDataReader reader = cmd.ExecuteReader();
+                var reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    QAs qa = new QAs
+                    var qa = new QAs
                     {
                         Qaid = reader.GetInt32(0),
                         StudentId = reader.GetInt32(1),
-                        CourseId = reader.GetString(2),
-                        Question = reader.GetString(3),
-                        Answer = reader.GetString(4),
-                        FeedbackDate = reader.GetDateTime(5)
+                        Student = new Students
+                        {
+                            StudentId = reader.GetInt32(1),
+                            StudentName = reader.GetString(2)
+                        },
+                        CourseId = reader.GetString(3),
+                        Course = new Courses
+                        {
+                            CourseId = reader.GetString(3),
+                            CourseTitle = reader.GetString(4)
+                        },
+                        Question = reader.GetString(5),
+                        Answer = reader.GetString(6),
+                        FeedbackDate = reader.GetDateTime(7)
                     };
                     qasList.Add(qa);
                 }
+
                 reader.Dispose();
                 con.Close();
             }
@@ -123,25 +153,27 @@ public class QAs
         {
             Console.WriteLine(ex.Message);
         }
+
         return qasList;
     }
+
     public QAs FetchQaById(int qaId)
     {
         QAs qa = null;
         try
         {
-            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            using (var con = new OracleConnection(ValuesConstants.DbString))
             {
-                string queryString = "SELECT Qaid, StudentId, CourseID, Question, Answer, FeedbackDate FROM QAs WHERE Qaid = :Qaid";
-                OracleCommand cmd = new OracleCommand(queryString, con);
+                var queryString =
+                    "SELECT Qaid, StudentId, CourseID, Question, Answer, FeedbackDate FROM QA WHERE Qaid = :Qaid";
+                var cmd = new OracleCommand(queryString, con);
                 cmd.Parameters.Add("Qaid", OracleDbType.Int32).Value = qaId;
                 cmd.BindByName = true;
                 cmd.CommandType = CommandType.Text;
 
                 con.Open();
-                OracleDataReader reader = cmd.ExecuteReader();
+                var reader = cmd.ExecuteReader();
                 if (reader.Read())
-                {
                     qa = new QAs
                     {
                         Qaid = reader.GetInt32(0),
@@ -151,7 +183,6 @@ public class QAs
                         Answer = reader.GetString(4),
                         FeedbackDate = reader.GetDateTime(5)
                     };
-                }
                 reader.Dispose();
                 con.Close();
             }
@@ -160,7 +191,61 @@ public class QAs
         {
             Console.WriteLine(ex.Message);
         }
+
         return qa;
     }
 
+    public async Task<List<QAs>> SearchQAsAsync(string searchTerm)
+    {
+        var qasList = new List<QAs>();
+        try
+        {
+            using (var con = new OracleConnection(ValuesConstants.DbString))
+            {
+                var queryString = @"
+                SELECT 
+                    q.Qaid, 
+                    s.StudentName, 
+                    c.CourseName, 
+                    q.Question, 
+                    q.Answer, 
+                    q.FeedbackDate
+                FROM 
+                    QAS q
+                JOIN 
+                    STUDENTS s ON q.StudentId = s.StudentId
+                JOIN 
+                    COURSES c ON q.CourseId = c.CourseId
+                WHERE 
+                    UPPER(s.StudentName) LIKE UPPER(:SearchTerm)
+                ORDER BY 
+                    s.StudentName";
+                var cmd = new OracleCommand(queryString, con);
+                cmd.Parameters.Add("SearchTerm", OracleDbType.Varchar2).Value = "%" + searchTerm + "%";
+                cmd.BindByName = true;
+                cmd.CommandType = CommandType.Text;
+
+                await con.OpenAsync();
+                var reader = await cmd.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                    qasList.Add(new QAs
+                    {
+                        Qaid = reader.GetInt32(0),
+                        Student = new Students { StudentName = reader.GetString(1) },
+                        Course = new Courses { CourseTitle = reader.GetString(2) },
+                        Question = reader.GetString(3),
+                        Answer = reader.GetString(4),
+                        FeedbackDate = reader.GetDateTime(5)
+                    });
+                reader.Dispose();
+                await con.CloseAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        return qasList;
+    }
 }
