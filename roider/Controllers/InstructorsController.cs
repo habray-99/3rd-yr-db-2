ae8b15af11d1468dac0d61a438f63ac9eefb7982
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-19 22:35:23 +0545

    Refactor InstructorsController

--- roider/Controllers/InstructorsController.cs
+++ roider/Controllers/InstructorsController.cs
@@ -1,131 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using Oracle.ManagedDataAccess.Client;
+﻿using Microsoft.AspNetCore.Mvc;
 using roider.Models;
 
-namespace roider.Controllers
+namespace roider.Controllers;
+
+public class InstructorsController : Controller
 {
-    public class InstructorsController : Controller
+    private readonly Instructors _instructorsModel = new();
+
+    // GET: Instructors
+    public IActionResult Index()
     {
-        private readonly Instructors _instructorsModel = new();
+        var instructorsList = _instructorsModel.FetchInstructors();
+        return View(instructorsList);
+    }
 
-        // GET: Instructors
-        public IActionResult Index()
+    // GET: Instructors/Create
+    public IActionResult Create()
+    {
+        var CountriesList = new Country().GetCountries();
+        ViewBag.CountriesList = CountriesList; // Changed to "CountriesList"
+        return View();return View();
+    }
+
+    // POST: Instructors/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Instructors instructor)
+    {
+        if (ModelState.IsValid)
         {
-            List<Instructors> instructorsList = _instructorsModel.FetchInstructors();
-            return View(instructorsList);
-        }
-
-        // GET: Instructors/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Instructors/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Instructors instructor)
-        {
-            if (ModelState.IsValid)
-            {
-                _instructorsModel.AddInstructor(instructor);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(instructor);
-        }
-
-        // GET: Instructors/Edit/5
-        public IActionResult Edit(string id)
-        {
-            if (id == null) // Corrected condition
-            {
-                return NotFound();
-            }
-
-            var instructor = _instructorsModel.FetchInstructorById(id);
-            if (instructor == null)
-            {
-                return NotFound();
-            }
-            return View(instructor);
-        }
-
-        // POST: Instructors/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(string id, Instructors instructor)
-        {
-            if (id != instructor.InstructorId)
-            {
-                return NotFound();
-            }
-
-            if (!ModelState.IsValid) return View(instructor);
-            _instructorsModel.EditInstructor(instructor, id);
+            _instructorsModel.AddInstructor(instructor);
             return RedirectToAction(nameof(Index));
         }
 
-        // GET: Instructors/Delete/5
-        public IActionResult Delete(string id)
-        {
-            if (id == null) // Corrected condition
-            {
-                return NotFound();
-            }
+        return View(instructor);
+    }
 
-            var instructor = _instructorsModel.FetchInstructorById(id);
-            if (instructor == null)
-            {
-                return NotFound();
-            }
+    // GET: Instructors/Edit/5
+    public IActionResult Edit(string id)
+    {
+        if (id == null) // Corrected condition
+            return NotFound();
 
-            return View(instructor);
-        }
+        var instructor = _instructorsModel.FetchInstructorById(id);
+        if (instructor == null) return NotFound();
+        return View(instructor);
+    }
 
-        // POST: Instructors/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(string id)
-        {
-            _instructorsModel.DeleteInstructor(id);
-            return RedirectToAction(nameof(Index));
-        }
+    // POST: Instructors/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(string id, Instructors instructor)
+    {
+        if (id != instructor.InstructorId) return NotFound();
 
-        // GET: Instructors/Details/5
-        public IActionResult Details(string? id)
-        {
-            if (id == null) // Corrected condition
-            {
-                return NotFound();
-            }
+        if (!ModelState.IsValid) return View(instructor);
+        _instructorsModel.EditInstructor(instructor, id);
+        return RedirectToAction(nameof(Index));
+    }
 
-            var instructor = _instructorsModel.FetchInstructorById(id);
-            if (instructor == null)
-            {
-                return NotFound();
-            }
+    // GET: Instructors/Delete/5
+    public IActionResult Delete(string id)
+    {
+        if (id == null) // Corrected condition
+            return NotFound();
 
-            return View(instructor);
-        }
+        var instructor = _instructorsModel.FetchInstructorById(id);
+        if (instructor == null) return NotFound();
+
+        return View(instructor);
+    }
+
+    // POST: Instructors/Delete/5
+    [HttpPost]
+    [ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(string id)
+    {
+        _instructorsModel.DeleteInstructor(id);
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: Instructors/Details/5
+    public IActionResult Details(string? id)
+    {
+        if (id == null) // Corrected condition
+            return NotFound();
+
+        var instructor = _instructorsModel.FetchInstructorById(id);
+        if (instructor == null) return NotFound();
+
+        return View(instructor);
+    }
+
     public async Task<IActionResult> SearchInstructors(string searchTerm)
     {
-        if (string.IsNullOrWhiteSpace(searchTerm))
-        {
-            return NotFound();
-        }
+        if (string.IsNullOrWhiteSpace(searchTerm)) return NotFound();
 
         var instructors = await _instructorsModel.SearchInstructorsAsync(searchTerm);
 
-        if (instructors == null)
-        {
-            return NotFound();
-        }
+        if (instructors == null) return NotFound();
 
         return PartialView("_InstructorList", instructors);
     }
-    }
-
-
 }
--- roider/Models/Instructors.cs
+++ roider/Models/Instructors.cs
@@ -6,24 +6,27 @@ namespace roider.Models;
 
 public class Instructors
 {
-    public List<Instructors> InstructorsList = [];
-    public int InstructorId { get; init; }
-    public string InstructorName { get; init; }
-    public string Contact { get; init; }
-    public string EmailAddress { get; init; }
-    public string Specialization { get; init; }
-    public int YearsOfExperience { get; init; }
-    public string Country { get; init; }
-    
+    public List<Instructors>? InstructorsList = [];
+    public string? InstructorId { get; set; }
+    public string? InstructorName { get; set; }
+    public string? Contact { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? Specialization { get; set; }
+    public int? YearsOfExperience { get; set; }
+    public string? Country { get; set; }
+
+    public List<Courses>? Courses { get; set; } = [];
+
     public void AddInstructor(Instructors instructor)
     {
         try
         {
-            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            using (var con = new OracleConnection(ValuesConstants.DbString))
             {
-                string queryString = "INSERT INTO Instructors (InstructorID, InstructorName, Contact, EmailAddress, Specialization, YearsOfExperience, Country) VALUES (:InstructorID, :InstructorName, :Contact, :EmailAddress, :Specialization, :YearsOfExperience, :Country)";
-                OracleCommand cmd = new OracleCommand(queryString, con);
-                cmd.Parameters.Add("InstructorID", OracleDbType.Int32).Value = instructor.InstructorId;
+                var queryString =
+                    "INSERT INTO Instructors (InstructorID, InstructorName, Contact, EmailAddress, Specialization, YearsOfExperience, Country_CODE) VALUES (:InstructorID, :InstructorName, :Contact, :EmailAddress, :Specialization, :YearsOfExperience, :Country)";
+                var cmd = new OracleCommand(queryString, con);
+                cmd.Parameters.Add("InstructorID", OracleDbType.Varchar2).Value = instructor.InstructorId;
                 cmd.Parameters.Add("InstructorName", OracleDbType.Varchar2).Value = instructor.InstructorName;
                 cmd.Parameters.Add("Contact", OracleDbType.Varchar2).Value = instructor.Contact;
                 cmd.Parameters.Add("EmailAddress", OracleDbType.Varchar2).Value = instructor.EmailAddress;
@@ -41,14 +44,17 @@ public class Instructors
             Console.WriteLine(ex.Message);
         }
     }
-    public void EditInstructor(Instructors instructor, int oldInstructorId)
+
+    public void EditInstructor(Instructors instructor, string oldInstructorId)
     {
         try
         {
-            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            using (var con = new OracleConnection(ValuesConstants.DbString))
             {
-                string queryString = "UPDATE Instructors SET InstructorName = :InstructorName, Contact = :Contact, EmailAddress = :EmailAddress, Specialization = :Specialization, YearsOfExperience = :YearsOfExperience, Country = :Country WHERE InstructorID = :OldInstructorID";
-                OracleCommand cmd = new OracleCommand(queryString, con);
+                var queryString =
+                    "UPDATE Instructors SET InstructorID = :InstructorID,InstructorName = :InstructorName, Contact = :Contact, EmailAddress = :EmailAddress, Specialization = :Specialization, YearsOfExperience = :YearsOfExperience, Country_CODE = :Country WHERE InstructorID = :OldInstructorID";
+                var cmd = new OracleCommand(queryString, con);
+                cmd.Parameters.Add("InstructorID", OracleDbType.Varchar2).Value = instructor.InstructorId;
                 cmd.Parameters.Add("InstructorName", OracleDbType.Varchar2).Value = instructor.InstructorName;
                 cmd.Parameters.Add("Contact", OracleDbType.Varchar2).Value = instructor.Contact;
                 cmd.Parameters.Add("EmailAddress", OracleDbType.Varchar2).Value = instructor.EmailAddress;
@@ -67,15 +73,16 @@ public class Instructors
             Console.WriteLine(ex.Message);
         }
     }
-    public void DeleteInstructor(int instructorId)
+
+    public void DeleteInstructor(string instructorId)
     {
         try
         {
-            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            using (var con = new OracleConnection(ValuesConstants.DbString))
             {
-                string queryString = "DELETE FROM Instructors WHERE InstructorID = :InstructorID";
-                OracleCommand cmd = new OracleCommand(queryString, con);
-                cmd.Parameters.Add("InstructorID", OracleDbType.Int32).Value = instructorId;
+                var queryString = "DELETE FROM Instructors WHERE InstructorID = :InstructorID";
+                var cmd = new OracleCommand(queryString, con);
+                cmd.Parameters.Add("InstructorID", OracleDbType.Varchar2).Value = instructorId;
 
                 con.Open();
                 cmd.ExecuteNonQuery();
@@ -87,25 +94,27 @@ public class Instructors
             Console.WriteLine(ex.Message);
         }
     }
+
     public List<Instructors> FetchInstructors()
     {
-        List<Instructors> instructorsList = new List<Instructors>();
+        var instructorsList = new List<Instructors>();
         try
         {
-            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            using (var con = new OracleConnection(ValuesConstants.DbString))
             {
-                string queryString = "SELECT InstructorID, InstructorName, Contact, EmailAddress, Specialization, YearsOfExperience, Country FROM Instructors";
-                OracleCommand cmd = new OracleCommand(queryString, con);
+                var queryString =
+                    "SELECT InstructorID, InstructorName, Contact, EmailAddress, Specialization, YearsOfExperience, Country_CODE FROM Instructors";
+                var cmd = new OracleCommand(queryString, con);
                 cmd.BindByName = true;
                 cmd.CommandType = CommandType.Text;
 
                 con.Open();
-                OracleDataReader reader = cmd.ExecuteReader();
+                var reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    Instructors instructor = new Instructors
+                    var instructor = new Instructors
                     {
-                        InstructorId = reader.GetInt32(0),
+                        InstructorId = reader.GetString(0),
                         InstructorName = reader.GetString(1),
                         Contact = reader.GetString(2),
                         EmailAddress = reader.GetString(3),
@@ -115,6 +124,7 @@ public class Instructors
                     };
                     instructorsList.Add(instructor);
                 }
+
                 reader.Dispose();
                 con.Close();
             }
@@ -123,28 +133,30 @@ public class Instructors
         {
             Console.WriteLine(ex.Message);
         }
+
         return instructorsList;
     }
-    public Instructors FetchInstructorById(int instructorId)
+
+    public Instructors FetchInstructorById(string? instructorId)
     {
         Instructors instructor = null;
         try
         {
-            using (OracleConnection con = new OracleConnection(ValuesConstants.DbString))
+            using (var con = new OracleConnection(ValuesConstants.DbString))
             {
-                string queryString = "SELECT InstructorID, InstructorName, Contact, EmailAddress, Specialization, YearsOfExperience, Country FROM Instructors WHERE InstructorID = :InstructorID";
-                OracleCommand cmd = new OracleCommand(queryString, con);
-                cmd.Parameters.Add("InstructorID", OracleDbType.Int32).Value = instructorId;
+                var queryString =
+                    "SELECT InstructorID, InstructorName, Contact, EmailAddress, Specialization, YearsOfExperience, Country_CODE FROM Instructors WHERE InstructorID = :InstructorID";
+                var cmd = new OracleCommand(queryString, con);
+                cmd.Parameters.Add("InstructorID", OracleDbType.Varchar2).Value = instructorId;
                 cmd.BindByName = true;
                 cmd.CommandType = CommandType.Text;
 
                 con.Open();
-                OracleDataReader reader = cmd.ExecuteReader();
+                var reader = cmd.ExecuteReader();
                 if (reader.Read())
-                {
                     instructor = new Instructors
                     {
-                        InstructorId = reader.GetInt32(0),
+                        InstructorId = reader.GetString(0),
                         InstructorName = reader.GetString(1),
                         Contact = reader.GetString(2),
                         EmailAddress = reader.GetString(3),
@@ -152,7 +164,6 @@ public class Instructors
                         YearsOfExperience = reader.GetInt32(5),
                         Country = reader.GetString(6)
                     };
-                }
                 reader.Dispose();
                 con.Close();
             }
@@ -161,7 +172,59 @@ public class Instructors
         {
             Console.WriteLine(ex.Message);
         }
+
         return instructor;
     }
 
+    public async Task<List<Instructors>> SearchInstructorsAsync(string searchTerm)
+    {
+        var instructorsList = new List<Instructors>();
+        try
+        {
+            using (var con = new OracleConnection(ValuesConstants.DbString))
+            {
+                var queryString = @"
+                SELECT 
+                    i.InstructorId, 
+                    i.InstructorName, 
+                    i.Contact, 
+                    i.EmailAddress, 
+                    i.Specialization, 
+                    i.YearsOfExperience, 
+                    i.Country
+                FROM 
+                    INSTRUCTORS i
+                WHERE 
+                    UPPER(i.InstructorName) LIKE UPPER(:SearchTerm)
+                ORDER BY 
+                    i.InstructorName";
+                var cmd = new OracleCommand(queryString, con);
+                cmd.Parameters.Add("SearchTerm", OracleDbType.Varchar2).Value = "%" + searchTerm + "%";
+                cmd.BindByName = true;
+                cmd.CommandType = CommandType.Text;
+
+                await con.OpenAsync();
+                var reader = await cmd.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                    instructorsList.Add(new Instructors
+                    {
+                        InstructorId = reader.GetString(0),
+                        InstructorName = reader.GetString(1),
+                        Contact = reader.GetString(2),
+                        EmailAddress = reader.GetString(3),
+                        Specialization = reader.GetString(4),
+                        YearsOfExperience = reader.GetInt32(5),
+                        Country = reader.GetString(6)
+                    });
+                reader.Dispose();
+                await con.CloseAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        return instructorsList;
+    }
 }
